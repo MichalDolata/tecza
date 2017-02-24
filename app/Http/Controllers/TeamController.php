2980f8d43a3ff321a0bd7aa5131001c2ba800d38
MichalDolata <dev@dolata.me>
2017-02-24 18:37:23 +0100

    Prepare file structure

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Team;
 
 class TeamController extends Controller
 {
-    //
+    public function index() {
+        $teams = Team::all();
+        return view('pages.teams.index', compact('teams'));
+    }
+
+    public function show(Team $team) {
+        return view('pages.teams.show', compact('team'));
+    }
 }
